6f4a7d96d2d93dbab6dc62dd757a3c967c5a247d
Ezhnya <161045816+Ezhnya@users.noreply.github.com>
2025-08-23 10:16:16 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import logging
+from aiogram import Bot, Dispatcher, executor
+from config import TOKEN
+from handlers import register_handlers
+from scheduler import setup_scheduler
+from database import init_db
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=8362036862:AAH0lDjFSfep9_-6DDVlFR8D_cXBcdMKDIQ)
+dp = Dispatcher(bot)
+
+# Реєструємо хендлери
+register_handlers(dp)
+
+# Підключаємо планувальник
+setup_scheduler(dp, bot)
+
+if __name__ == "__main__":
+    init_db()
+    executor.start_polling(dp, skip_updates=True)
